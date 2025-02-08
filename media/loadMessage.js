cdca4d550031394448ac35aa01c7286fda57231d
Cheeseworks <bluewitherer2422@gmail.com>
2025-02-08 16:51:23 -0500

    adjustments

--- media/loadMessage.js
+++
@@ -1,9 +0,0 @@
-function loadCommonBody() {
-    fetch('message.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.innerHTML = data;
-        });
-};
-
-window.onload = loadCommonBody;
